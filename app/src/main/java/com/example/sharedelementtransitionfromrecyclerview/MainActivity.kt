11bd06dba63bf2a2ababe178332436ab5261671c
bgarcia <bgarcia1832@gmail.com>
2020-01-02 19:14:53 -0600

    GetPhotosFromWebsite function added

--- app/src/main/java/com/example/sharedelementtransitionfromrecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/sharedelementtransitionfromrecyclerview/MainActivity.kt
@@ -8,5 +8,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getPhotosFromWebsite()
+
     }
+
+    //Retrieve 20 photos from the website picsum.photos
+    private fun getPhotosFromWebsite() = (100..120)
+        .map{"https://picsum.photos/1000/700?image=$it"}
 }
